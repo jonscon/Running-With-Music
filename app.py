171753a271e7ae7933df76bdfb3afa9c423f97c5
JonC <jonathancheng@Jonathans-MacBook-Pro-2.local>
2024-06-29 16:03:08 -0700

    modified database URL

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'postgresql:///running-app')
+    'DATABASE_URL', 'postgresql://postgres.roaatkovyyztyhfwwnxc:[xZd3BP29sSceddQB]@aws-0-us-west-1.pooler.supabase.com:6543/postgres')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
